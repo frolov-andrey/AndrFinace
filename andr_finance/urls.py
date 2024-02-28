a65eb74604bf6620936bac1792ce24b1fbbbe513
Андрей Фролов <afrolov27@gmail.com>
2024-02-28 15:32:31 +0600

    error

--- andr_finance/urls.py
+++ andr_finance/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('accounts/', views.accounts, name='accounts'),
-    path('accounts/', views.accounts, name='accounts'),
 
     path('categories/', views.categories, name='categories'),
     path('new_category/', views.new_category, name='new_category'),
