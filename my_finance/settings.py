7d0cea6def1535566bb322dda8b92ec78c12da4c
Андрей Фролов <afrolov27@gmail.com>
2024-03-25 20:04:43 +0500

    settings.py

--- my_finance/settings.py
+++ my_finance/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'TIME_ZONE'
 
 USE_I18N = True
 
