ab79908605a254e7de8a1dfa2c76e963f92c5533
Андрей Фролов <afrolov27@gmail.com>
2024-03-27 22:03:59 +0500

    Добавил индексы

--- my_finance/andr_finance/models.py
+++ my_finance/andr_finance/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True, db_index=True)
     icon_folder = models.FilePathField(
         allow_folders=True, allow_files=False, verbose_name='Папка иконки',
         default=None, null=True, blank=True)
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 
 class Account(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Название')
+    name = models.CharField(max_length=200, verbose_name='Название', db_index=True)
     start_balance = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Начальный баланс')
     icon_folder = models.FilePathField(
         allow_folders=True, allow_files=False, verbose_name='Папка иконки',
@@ -29,13 +29,15 @@ class Transaction(models.Model):
         Account,
         on_delete=models.DO_NOTHING,
         related_name='accounts',
-        verbose_name='Счет'
+        verbose_name='Счет',
+        db_index=True
     )
     account_recipient = models.ForeignKey(
         Account, on_delete=models.DO_NOTHING,
         related_name='account_recipient',
         blank=True, null=True,
-        verbose_name='Счет получатель'
+        verbose_name='Счет получатель',
+        db_index=True
     )
 
     MINUS = 'minus'
@@ -51,16 +53,18 @@ class Transaction(models.Model):
         max_length=20,
         choices=TYPE_TRANSACTION,
         default=MINUS,
-        verbose_name='Тип транзакции'
+        verbose_name='Тип транзакции',
+        db_index=True,
     )
-    amount = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Сумма')
+    amount = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Сумма', db_index=True)
     category = models.ForeignKey(
         Category,
         on_delete=models.DO_NOTHING,
         blank=True, null=True,
-        verbose_name='Категория'
+        verbose_name='Категория',
+        db_index=True,
     )
-    date_added = models.DateTimeField()
+    date_added = models.DateTimeField(db_index=True)
     title = models.CharField(max_length=200)
 
     def __str__(self):
