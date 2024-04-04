a06b9d77cf00dedf21e468b8381ff44267a2319c
Андрей Фролов <afrolov27@gmail.com>
2024-04-04 21:26:46 +0500

    Создал приложение Users

--- my_finance/my_finance/settings.py
+++ my_finance/my_finance/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'andr_finance',
+    'users',
 ]
 
 MIDDLEWARE = [
---
+++ my_finance/users/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def login_user(request):
+    return HttpResponse("login")
+
+
+def logout_user(request):
+    return HttpResponse("logout")
