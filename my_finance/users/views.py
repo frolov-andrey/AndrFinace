d36723e7a8e4d9754861cc4574d902bb2867d9da
Андрей Фролов <afrolov27@gmail.com>
2024-04-05 00:28:45 +0500

    #62. Классы LoginView, LogoutView и AuthenticationForm | Уроки по Django 4

--- my_finance/users/views.py
+++ my_finance/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -6,20 +7,28 @@ from django.urls import reverse
 from .forms import LoginUserForm
 
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'],
-                                password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('andr_finance:index'))
-    else:
-        form = LoginUserForm()
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
 
-    return render(request, 'users/login.html', {'form': form})
+    # def get_success_url(self):
+    #     return reverse_lazy('home')
+
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'],
+#                                 password=cd['password'])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse('andr_finance:index'))
+#     else:
+#         form = LoginUserForm()
+#
+#     return render(request, 'users/login.html', {'form': form})
 
 
 def logout_user(request):
