8e0ee3d4c93b6ea53ab1d81eb692fa81e800c641
Андрей Фролов <afrolov27@gmail.com>
2024-04-08 05:09:37 +0500

    #64. Регистрация пользователей через функции представления | Уроки по Django 4

--- my_finance/users/views.py
+++ my_finance/users/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -15,25 +15,21 @@ class LoginUser(LoginView):
         'select_menu': 'login'
     }
 
-    # def get_success_url(self):
-    #     return reverse_lazy('home')
-
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'],
-#                                 password=cd['password'])
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('andr_finance:index'))
-#     else:
-#         form = LoginUserForm()
-#
-#     return render(request, 'users/login.html', {'form': form})
-
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('users:login'))
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
